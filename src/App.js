2db3a535a7b1b9562917f99f27e4ad4b06df1190
DesmondTo <e0550619@u.nus.edu>
2022-05-19 16:14:31 +0800

    Setup page routing and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoadingPage from "./pages/LoadingPage";
+const GamePage = React.lazy(() => import("./pages/GamePage"));
+const GameRulePage = React.lazy(() => import("./pages/GameRulePage"));
+const NotFoundPage = React.lazy(() => import("./pages/NotFoundPage"));
 
 function App() {
-  return <div>// TODO: Implement routing and Material UI theming here.</div>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route path="/" element={<GamePage />} />
+          <Route path="game-rule" element={<GameRulePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoadingPage(props) {
+  return <div>// TODO: Implement a Loading Page.</div>;
+}
+
+export default LoadingPage;
