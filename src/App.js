0888d6753eefab18fa892698580e9ee7daa083c2
DesmondTo <e0550619@u.nus.edu>
2022-05-21 10:50:54 +0800

    Format code

--- src/App.js
+++ src/App.js
@@ -9,23 +9,21 @@ const GamePage = React.lazy(() => import("./pages/GamePage"));
 const GameRulePage = React.lazy(() => import("./pages/GameRulePage"));
 const NotFoundPage = React.lazy(() => import("./pages/NotFoundPage"));
 
-
 function App(props) {
   const theme = useTheme();
   const colorMode = React.useContext(props.colorModeContext);
 
   return (
-    
-      <BrowserRouter>
-        <Navbar theme={theme} handleMode={colorMode.toggleColorMode} />
-        <Suspense fallback={<LoadingPage />}>
-          <Routes>
-            <Route path="/" element={<GamePage />} />
-            <Route path="game-rule" element={<GameRulePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar theme={theme} handleMode={colorMode.toggleColorMode} />
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route path="/" element={<GamePage />} />
+          <Route path="game-rule" element={<GameRulePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
