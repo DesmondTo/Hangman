414f2380bf3fff4bbd3342e411103ea9fa2d9428
DesmondTo <e0550619@u.nus.edu>
2022-05-22 08:38:33 +0800

    Implement Game Rule page

--- src/pages/GameRulePage.js
+++ src/pages/GameRulePage.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+// Import my component here
 
 function GameRulePage(props) {
-    return <div>// TODO: Implement a Game Rule page.</div>;
+  return (
+    <div>
+      <h1>// Use my component here.</h1>
+      // TODO: Implement a Game Rule page.
+    </div>
+  );
 }
 
 export default GameRulePage;
