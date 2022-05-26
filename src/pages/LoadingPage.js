0ebea09c8aef88ae3a83c0e1871bf2b5f54b2abb
thebadone231 <chenhaoli123@gmail.com>
2022-05-27 00:06:32 +0800

    added animation into LoadingPage

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React from 'react';
+import LoadingSpin from 'react-loading-spin';
 
 function LoadingPage(props) {
-  return <div>// TODO: Implement a Loading Page.</div>;
+  const LoadingSpinner = () => (
+    <div className={'Spinner'}>
+      <LoadingSpin size="250px" primaryColor="black" />
+    </div>
+  );
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '30vh',
+        }}
+      >
+        <h1>
+          Preparing to hang the man...do you have what it takes to save him?
+        </h1>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '30vh',
+        }}
+      >
+        <LoadingSpinner />
+      </div>
+    </div>
+  );
 }
 
 export default LoadingPage;
