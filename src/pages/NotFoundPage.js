22c294f86b66f2b54019ba1c7a012e1a24192774
DesmondTo <e0550619@u.nus.edu>
2022-05-26 23:38:37 +0800

    Use Material UI button

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,5 +1,9 @@
 import React from "react";
+
 import { useNavigate } from "react-router-dom";
+
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function NotFoundPage(props) {
   let navigate = useNavigate();
@@ -8,7 +12,9 @@ function NotFoundPage(props) {
     <div>
       <h1>Error 404 : Not Found</h1>
       <p>
-        <button onClick={() => navigate("/")}>Home</button>
+        <Button onClick={() => navigate("/")}>
+          <Typography>Home</Typography>
+        </Button>
       </p>
     </div>
   );
